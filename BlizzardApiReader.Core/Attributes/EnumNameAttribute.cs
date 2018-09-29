3a5aca9fb8e4d84279a3b5c5c7933e28dc987ba6
linahanner <lina.hannersanchez@gmail.com>
2018-09-29 07:29:41 +0200

    Add attribute EnumName to specify name of Enum value

---
+++ BlizzardApiReader.Core/Attributes/EnumNameAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace BlizzardApiReader.Core.Attributes
+{
+    public class EnumNameAttribute : Attribute
+    {
+        public EnumNameAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+
+    public static class EnumNameAttributeExtensions
+    {
+        public static string GetEnumName<TEnum>(this TEnum value) where TEnum : Enum
+        {
+            Type type = typeof(TEnum);
+            var field = type.GetField(value.ToString());
+            var attr = field.GetCustomAttribute(typeof(EnumNameAttribute)) as EnumNameAttribute;
+
+            return attr?.Name;
+        }
+    }
+}
