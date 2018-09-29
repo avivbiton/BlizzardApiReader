dc09661a8507d7bca15e9d25b9500849910bb454
linahanner <lina.hannersanchez@gmail.com>
2018-09-29 08:24:08 +0200

    Add test project with tests for ApiConfiguration constructor

---
+++ BlizzardApiReader.Tests/ApiConfigurationTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using BlizzardApiReader.Core.Enums;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlizzardApiReader.Tests
+{
+    [TestClass]
+    public class ApiConfigurationTests
+    {
+        #region Constructor
+        [TestMethod]
+        public void Constructor_NoParameters_DefaultConfigurations()
+        {
+            var configuration = new ApiConfiguration();
+
+            configuration.ApiRegion.Should().BeEquivalentTo(Region.UnitedStates);
+            configuration.ResultLocale.Should().BeEquivalentTo(Locale.AmericanEnglish);
+        }
+
+        [TestMethod]
+        public void Constructor_ApiKey_DefaultConfigurations()
+        {
+            var configuration = new ApiConfiguration("anyapikey");
+
+            configuration.ApiRegion.Should().BeEquivalentTo(Region.UnitedStates);
+            configuration.ResultLocale.Should().BeEquivalentTo(Locale.AmericanEnglish);
+        }
+
+        [TestMethod]
+        public void Constructor_Region_DefaultLocale()
+        {
+            var configuration = new ApiConfiguration(Region.Europe, "anyapikey");
+
+            configuration.ApiRegion.Should().BeEquivalentTo(Region.Europe);
+            configuration.ResultLocale.Should().BeEquivalentTo(Locale.BritishEnglish);
+        }
+
+        [TestMethod]
+        public void Constructor_AllParameters_Configurations()
+        {
+            var configuration = new ApiConfiguration(Region.Europe, Locale.Korean, "anyapikey");
+
+            configuration.ApiRegion.Should().BeEquivalentTo(Region.Europe);
+            configuration.ResultLocale.Should().BeEquivalentTo(Locale.Korean);
+        }
+        #endregion
+
+        #region SetRegion
+        #endregion
+    }
+}
---
+++ BlizzardApiReader.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BlizzardApiReader.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BlizzardApiReader.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6ebaf3a0-2591-4f91-81a8-0fc1288f7f21")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
