41349df3e103598fa2821185bcf72a368b859540
linahanner <lina.hannersanchez@gmail.com>
2018-09-29 07:50:50 +0200

    Move EnumNameExtensions to Extensions folder

--- BlizzardApiReader.Core/Attributes/EnumNameAttribute.cs
+++ BlizzardApiReader.Core/Attributes/EnumNameAttribute.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Reflection;
 
 namespace BlizzardApiReader.Core.Attributes
 {
@@ -11,17 +10,5 @@ namespace BlizzardApiReader.Core.Attributes
         }
 
         public string Name { get; set; }
-    }
-
-    public static class EnumNameAttributeExtensions
-    {
-        public static string GetEnumName<TEnum>(this TEnum value) where TEnum : Enum
-        {
-            Type type = typeof(TEnum);
-            var field = type.GetField(value.ToString());
-            var attr = field.GetCustomAttribute(typeof(EnumNameAttribute)) as EnumNameAttribute;
-
-            return attr?.Name;
-        }
     }
 }
