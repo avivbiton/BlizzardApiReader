75b5e89116b39ca1f3c3e0947adb97c5dbc7c474
linahanner <lina.hannersanchez@gmail.com>
2018-09-29 15:40:03 +0200

    Move RegionExtensions from ApiConfiguration to Core project

--- BlizzardApiReader/ApiConfiguration.cs
+++ BlizzardApiReader/ApiConfiguration.cs
@@ -133,30 +133,4 @@ namespace BlizzardApiReader
             return ApiRegion.GetEnumName();
         }
     }
-
-    public static class RegionExtensions
-    {
-        public static Locale GetDefaultLocale(this Region region)
-        {
-            switch (region)
-            {
-                case Region.EU:
-                case Region.Europe:
-                    return Locale.BritishEnglish;
-                case Region.KR:
-                case Region.Korea:
-                    return Locale.Korean;
-                case Region.TW:
-                case Region.Taiwan:
-                    return Locale.TraditionalChinese;
-                case Region.SEA:
-                case Region.SoutheastAsia:
-                case Region.US:
-                case Region.UnitedStates:
-                    return Locale.AmericanEnglish;
-                default:
-                    throw new NotImplementedException($"The {nameof(Region)} [{region.ToString()}] does not have an associated {nameof(Locale)}");
-            }
-        }
-    }
 }
