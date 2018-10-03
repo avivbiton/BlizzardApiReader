253745ab3c219c8aa170577d083324129f0c05cb
tomval <tomovaliuno@gmail.com>
2018-10-03 22:52:22 +0300

    Fixed api to call correct url. Added additional diablo 3 api calls

---
+++ BlizzardApiReader/Diablo/Models/Follower.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BlizzardApiReader.Diablo.Models
+{
+    public class Follower
+    {
+        [JsonProperty("slug")]
+        public string Slug { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("realName")]
+        public string RealName { get; set; }
+
+        [JsonProperty("portrait")]
+        public string Portrait { get; set; }
+
+        [JsonProperty("skills")]
+        public List<Skill> Skills { get; set; }
+    }
+
+    public class Skill
+    {
+        [JsonProperty("slug")]
+        public string Slug { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+
+        [JsonProperty("level")]
+        public long Level { get; set; }
+
+        [JsonProperty("tooltipUrl")]
+        public string TooltipUrl { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("descriptionHtml")]
+        public string DescriptionHtml { get; set; }
+    }
+}
---
+++ BlizzardApiReader/Diablo/Models/ItemType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BlizzardApiReader.Diablo.Models
+{
+    public class ItemType
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("path")]
+        public string Path { get; set; }
+    }
+}
