304a0a1576dad96b6585c0abf37a66e697a61a14
Christopher Johnson <CLJohnson088@gmail.com>
2018-10-06 09:39:44 -0500

    WoW Realm API

---
+++ BlizzardApiReader/WorldOfWarcaraft/Models/Realm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlizzardApiReader.WorldOfWarcaraft.Models
+{
+    public class Realm
+    {
+        public string type { get; set; }
+        public string population { get; set; }
+        public bool queue { get; set; }
+        public bool status { get; set; }
+        public string name { get; set; }
+        public string slug { get; set; }
+        public string battlegroup { get; set; }
+        public string locale { get; set; }
+        public string timezone { get; set; }
+        public string[] connected_realms { get; set; }
+    }
+}
---
+++ BlizzardApiReader/WorldOfWarcaraft/WorldOfWarcraftApi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlizzardApiReader.WorldOfWarcaraft.Models;
+
+namespace BlizzardApiReader.WorldOfWarcaraft
+{
+    public class WorldOfWarcraftApi
+    {
+        private ApiReader reader;
+
+        public WorldOfWarcraftApi()
+        {
+            reader = new ApiReader();
+        }
+
+        public WorldOfWarcraftApi(ApiConfiguration configuration)
+        {
+            reader = new ApiReader(configuration);
+        }
+
+        public void OverrideConfiguration(ApiConfiguration newConfiguration)
+        {
+            reader.Configuration = newConfiguration;
+        }
+
+        #region WoW Realm Status
+        public async Task<List<Realm>> GetRealmIndexAsync()
+        {
+            string query = $"/wow/realm/status";
+            var results = await reader.GetAsync<Dictionary<string, List<Realm>>>(query);
+            return results["realms"];
+        }
+        #endregion
+    }
+}
