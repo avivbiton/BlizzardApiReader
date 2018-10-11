127f10d4e547fcfa2bbc10ef36d6b5eabdcb2d76
Aviv Biton <avivbiton01@gmail.com>
2018-10-11 18:02:02 +0300

    Moved Spell model to it's file

--- BlizzardApiReader.Diablo/Models/Follower.cs
+++ BlizzardApiReader.Diablo/Models/Follower.cs
@@ -16,14 +16,4 @@ namespace BlizzardApiReader.Diablo.Models
         public List<Skill> Skills { get; set; }
     }
 
-    public class Skill
-    {
-        public string Slug { get; set; }
-        public string Name { get; set; }
-        public string Icon { get; set; }
-        public long Level { get; set; }
-        public string TooltipUrl { get; set; }
-        public string Description { get; set; }
-        public string DescriptionHtml { get; set; }
-    }
 }
---
+++ BlizzardApiReader.Diablo/Models/Skill.cs
@@ -0,0 +1,13 @@
+﻿namespace BlizzardApiReader.Diablo.Models
+{
+    public class Skill
+    {
+        public string Slug { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public long Level { get; set; }
+        public string TooltipUrl { get; set; }
+        public string Description { get; set; }
+        public string DescriptionHtml { get; set; }
+    }
+}
