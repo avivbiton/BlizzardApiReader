7aa7e9ae7496db3729b42dde1aa0f4b1ca5b0b0d
Redmars27 <miguel.redmars@gmail.com>
2018-12-01 21:13:52 +0100

    Add all locale and update tests

--- BlizzardApiReader.Core.Tests/Extensions/RegionExtensionsTests.cs
+++ BlizzardApiReader.Core.Tests/Extensions/RegionExtensionsTests.cs
@@ -40,5 +40,53 @@ namespace BlizzardApiReader.Core.Tests.Extensions
             Region.UnitedStates.GetDefaultLocale().Should().BeEquivalentTo(Locale.AmericanEnglish);
         }
         #endregion
+
+        #region IsLocaleAvailable        
+        [DataTestMethod]
+        [DataRow(Region.UnitedStates,Locale.AmericanEnglish)]
+        [DataRow(Region.UnitedStates, Locale.MexicanSpanish)]
+        [DataRow(Region.UnitedStates, Locale.BrazilianPortuguese)]
+        [DataRow(Region.Europe, Locale.BritishEnglish)]
+        [DataRow(Region.Europe, Locale.SpainSpanish)]
+        [DataRow(Region.Europe, Locale.French)]
+        [DataRow(Region.Europe, Locale.Russian)]
+        [DataRow(Region.Europe, Locale.German)]
+        [DataRow(Region.Europe, Locale.PortugalPortuguese)]
+        [DataRow(Region.Europe, Locale.Italian)]
+        [DataRow(Region.Korea, Locale.Korean)]
+        [DataRow(Region.Taiwan, Locale.TraditionalChinese)]
+        [DataRow(Region.China, Locale.SimplifiedChinese)]
+        public void ValidLocaleForRegion(Region region, Locale locale)
+        {
+            region.IsLocaleAvailable(locale).Should().BeTrue();
+        }
+
+
+        [DataTestMethod]
+        [DataRow(Region.Europe, Locale.AmericanEnglish)]
+        [DataRow(Region.Korea, Locale.AmericanEnglish)]
+        [DataRow(Region.Taiwan, Locale.AmericanEnglish)]
+        [DataRow(Region.China, Locale.AmericanEnglish)]
+        [DataRow(Region.Europe, Locale.MexicanSpanish)]
+        [DataRow(Region.Korea, Locale.MexicanSpanish)]
+        [DataRow(Region.Taiwan, Locale.MexicanSpanish)]
+        [DataRow(Region.China, Locale.MexicanSpanish)]
+        [DataRow(Region.Europe, Locale.BrazilianPortuguese)]
+        [DataRow(Region.Korea, Locale.BrazilianPortuguese)]
+        [DataRow(Region.Taiwan, Locale.BrazilianPortuguese)]
+        [DataRow(Region.China, Locale.BrazilianPortuguese)]
+
+        [DataRow(Region.UnitedStates, Locale.BritishEnglish)]
+
+        [DataRow(Region.China, Locale.Korean)]
+
+        [DataRow(Region.Europe, Locale.SimplifiedChinese)]
+        public void InvalidLocaleForRegion(Region region, Locale locale)
+        {
+            region.IsLocaleAvailable(locale).Should().BeFalse();
+        }
+
+        #endregion
+
     }
 }
